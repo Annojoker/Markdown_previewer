5ea96fdbf37af53392989f3d920c67e4cb718e12
Annojoker <prethiv2004@gmail.com>
2024-03-30 02:08:43 +0530

    Changes some format and and edited the outline

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <h1>Markdown Previewer</h1>
+        <h1 style={{textAlign:'center'}}>Markdown Previewer</h1>
         <div className="boxes-container">
+          <h2>Input Area:</h2>
           <textarea name="editor" id="editor" value={markdowntext} onChange={(e) => setmarkdowntext(e.target.value)}></textarea>
+          <h2>Preview Area:</h2>
           <div id="preview">
             <ReactMarkdown>{markdowntext}</ReactMarkdown>
           </div>
